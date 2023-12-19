170e4127fa58fa64490fe50f6d170ddd188d1f91
Abrham007 <abrhamaraya111@gmail.com>
2023-12-19 14:17:29 +0300

    made the component more performant

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 function Button(props) {
   return (
     <button className={`Button btn-${props.type}`}>
-      <img src={props.icon} alt=""></img>
+      <img src={props.icon} alt="" width={9} height={9}></img>
       <span>{props.text}</span>
     </button>
   );
