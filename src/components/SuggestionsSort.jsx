0b904cfd5c75bb2c85d3149398d2833da1a28376
Abrham007 <abrhamaraya111@gmail.com>
2023-12-19 14:21:47 +0300

    fixed the svg bug added the drop menu

--- src/components/SuggestionsSort.jsx
+++ src/components/SuggestionsSort.jsx
@@ -1,20 +1,33 @@
+import DropMenu from "./DropMenu";
 import "./SuggestionsSort.css";
 
 function SuggestionsSort() {
+  const sortList = [
+    "Most Upvotes",
+    "Least Upvotes",
+    "Most Comments",
+    "Least Comments",
+  ];
   return (
-    <button className="SuggestionsSort">
-      <span>Sort by :</span>
-      <span className="SuggestionsSort__selected">Most Upvotes</span>
-      <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
-        <path
-          d="M1 1l4 4 4-4"
-          stroke="#FFF"
-          stroke-width="2"
-          fill="none"
-          fill-rule="evenodd"
-        />
-      </svg>
-    </button>
+    <div className="SuggestionsSort">
+      <button className="SuggestionsSort__btn">
+        <span>Sort by :</span>
+        <span className="SuggestionsSort__selected">Most Upvotes</span>
+        <svg width="10" height="7" xmlns="http://www.w3.org/2000/svg">
+          <path
+            d="M1 1l4 4 4-4"
+            stroke="#FFF"
+            strokeWidth="2"
+            fill="none"
+            fillRule="evenodd"
+          />
+        </svg>
+      </button>
+
+      {/* <div className="SuggestionsSort__drop-menu">
+        <DropMenu menuList={sortList} activeBtn={0} />
+      </div> */}
+    </div>
   );
 }
 
