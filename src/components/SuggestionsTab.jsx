aaf5b362e6708821fedcc8674c221d8a5107dd3d
Abrham007 <abrhamaraya111@gmail.com>
2023-12-19 10:16:58 +0300

    added the sort and btn components

--- src/components/SuggestionsTab.jsx
+++ src/components/SuggestionsTab.jsx
@@ -1,15 +1,18 @@
 import "./SuggestionsTab.css";
 import suggestionIcon from "../assets/shared/suggestions/icon-suggestions.svg";
+import SuggestionsSort from "./SuggestionsSort";
+import Button from "./Button";
+import plusIcon from "../assets/shared/icon-plus.svg";
 
 function SuggestionsTab() {
   return (
     <div className="SuggestionsTab">
       <div className="SuggestionsTab_header">
-        <img src={suggestionIcon}></img>
+        <img src={suggestionIcon} alt="" width={24} height={24}></img>
         <h2>2 Suggestions</h2>
       </div>
-      <div></div>
-      <button></button>
+      <SuggestionsSort />
+      <Button text="Add Feedback" icon={plusIcon} type={1} />
     </div>
   );
 }
