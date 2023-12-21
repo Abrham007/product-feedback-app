b134b2de66a5b719c6aad3829f5fa5e2d2d6dcea
Abrham007 <abrhamaraya111@gmail.com>
2023-12-21 15:40:17 +0300

    made this components more usable

--- src/components/FeedBackEmpty.jsx
+++ src/components/FeedBackEmpty.jsx
@@ -20,7 +20,23 @@ function FeedBackEmpty() {
           hearing about new ideas to improve our app.
         </p>
       </div>
-      <Button text="Add Feedback" icon={plusIcon} type={1} />
+      <Button
+        text="Add Feedback"
+        icon={
+          <>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="12"
+              viewBox="0 -960 960 960"
+              width="12"
+              fill="#F2F4FE"
+            >
+              <path d="M417-417H166v-126h251v-251h126v251h251v126H543v251H417v-251Z" />
+            </svg>
+          </>
+        }
+        type={1}
+      />
     </div>
   );
 }
--- src/components/ScoreButton.jsx
+++ src/components/ScoreButton.jsx
@@ -1,11 +1,24 @@
 import "./ScoreButton.css";
 import upIcon from "../assets/shared/icon-arrow-up.svg";
+import { useState } from "react";
 
 function ScoreButton(props) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  function handleClick() {
+    setIsClicked(true);
+    props.handleScore(props.id);
+  }
+
   return (
-    <button className="ScoreButton">
+    <button
+      style={isClicked ? { backgroundColor: "#4661e6", color: "#fff" } : {}}
+      className="ScoreButton"
+      onClick={handleClick}
+      disabled={isClicked}
+    >
       <svg
-        className="ScoreButton__icon"
+        style={{ stroke: isClicked ? "#fff" : "#4661E6" }}
         stroke="#4661E6"
         width="10"
         height="7"
@@ -13,7 +26,7 @@ function ScoreButton(props) {
       >
         <path d="M1 6l4-4 4 4" strokeWidth="2" fill="none" fillRule="evenodd" />
       </svg>
-      <span>{props.score}</span>
+      <span>{isClicked ? props.score + 1 : props.score}</span>
     </button>
   );
 }
