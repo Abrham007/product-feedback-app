3f5cb6cc2bc20484e94013d3d95b5b8b65b3c1ff
Abrham007 <abrhamaraya111@gmail.com>
2023-12-21 15:53:22 +0300

    almost finished with static version of this component

--- src/components/FeedBackDetail.jsx
+++ src/components/FeedBackDetail.jsx
@@ -2,13 +2,30 @@ import Button from "./Button";
 import "./FeedBackDetail.css";
 import FeedBackPost from "./FeedBackPost";
 import leftArrowIcon from "../assets/shared/icon-arrow-left.svg";
+import CommentList from "./CommentList";
 
 function FeedBackDetail(props) {
   return (
     <div className="FeedBackDetail">
       <div className="FeedBackDetail__header">
-        <Button icon={leftArrowIcon} type={5} />
-        <Button type={2} />
+        <Button
+          text={"Go Back"}
+          icon={
+            <>
+              <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M6 9L2 5l4-4"
+                  stroke="#647196"
+                  strokeWidth="2"
+                  fill="none"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </>
+          }
+          type={5}
+        />
+        <Button text={"Edit Feedback"} type={2} />
       </div>
       <FeedBackPost
         title={"Add tags for solutions"}
@@ -19,7 +36,7 @@ function FeedBackDetail(props) {
         category={"enhancement"}
         comments={[1, 1]}
       />
-      <div></div>
+      <CommentList />
     </div>
   );
 }
