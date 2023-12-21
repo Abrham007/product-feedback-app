214db29f58f8a9bb0dc7fa90e7424bb372b361b1
Abrham007 <abrhamaraya111@gmail.com>
2023-12-21 15:38:59 +0300

    made it responsive for mobile and implmented hambuger menu

--- src/components/CompanyName.jsx
+++ src/components/CompanyName.jsx
@@ -2,15 +2,21 @@ import "./CompanyName.css";
 import hamburgerIcon from "../assets/shared/mobile/icon-hamburger.svg";
 import closeIcon from "../assets/shared/mobile/icon-close.svg";
 
-function CompanyName() {
+function CompanyName(props) {
+  function handleClick() {
+    props.setIsOpen((prevValue) => !prevValue);
+  }
   return (
     <div className="CompanyName">
       <div>
         <h1 className="CompanyName__header">Frontend Mentor</h1>
         <p className="CompanyName__paragraph">Feedback Board</p>
       </div>
-      <button className="CompanyName__btn">
-        <img src={hamburgerIcon} alt="menu icon"></img>
+      <button onClick={handleClick} className="CompanyName__btn">
+        <img
+          src={props.isOpen ? closeIcon : hamburgerIcon}
+          alt="menu icon"
+        ></img>
       </button>
     </div>
   );
