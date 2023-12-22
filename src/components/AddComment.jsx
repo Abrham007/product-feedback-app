1363b9f46bf66b9192e9204d154b0c81c8f015dc
Abrham007 <abrhamaraya111@gmail.com>
2023-12-22 13:41:25 +0300

    finished this component for FeedBackDetail

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,13 +1,16 @@
-import "./AddComments.css";
+import "./AddComment.css";
 
 function AddComment() {
   return (
     <form className="AddComment">
       <h4 className="AddComment__header">Add Comment</h4>
-      <textarea className="AddComment__input"></textarea>
+      <textarea
+        className="AddComment__input"
+        placeholder="Type your comment here"
+      ></textarea>
       <div className="AddComment__box">
         <p className="AddComment__char">250 Characters left</p>
-        <button className="AddComment__btn"></button>
+        <button className="AddComment__btn">Post Comment</button>
       </div>
     </form>
   );
