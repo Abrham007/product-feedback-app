bdc3a96739a6be6ef8363597c015efe17be1936a
Abrham007 <abrhamaraya111@gmail.com>
2023-12-22 13:44:52 +0300

    added native button and removed Custom btn component for readablity

--- src/components/FeedBackDetail.jsx
+++ src/components/FeedBackDetail.jsx
@@ -1,31 +1,28 @@
-import Button from "./Button";
 import "./FeedBackDetail.css";
 import FeedBackPost from "./FeedBackPost";
 import leftArrowIcon from "../assets/shared/icon-arrow-left.svg";
 import CommentList from "./CommentList";
+import AddComment from "./AddComment";
 
 function FeedBackDetail(props) {
   return (
     <div className="FeedBackDetail">
       <div className="FeedBackDetail__header">
-        <Button
-          text={"Go Back"}
-          icon={
-            <>
-              <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
-                <path
-                  d="M6 9L2 5l4-4"
-                  stroke="#647196"
-                  strokeWidth="2"
-                  fill="none"
-                  fillRule="evenodd"
-                />
-              </svg>
-            </>
-          }
-          type={5}
-        />
-        <Button text={"Edit Feedback"} type={2} />
+        <button className="FeedBackDetail__btn FeedBackDetail__btn--1">
+          <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
+            <path
+              d="M6 9L2 5l4-4"
+              stroke="#647196"
+              strokeWidth="2"
+              fill="none"
+              fillRule="evenodd"
+            />
+          </svg>
+          <span>Go Back</span>
+        </button>
+        <button className="FeedBackDetail__btn FeedBackDetail__btn--2">
+          <span>Edit Feedback</span>
+        </button>
       </div>
       <FeedBackPost
         title={"Add tags for solutions"}
@@ -37,6 +34,7 @@ function FeedBackDetail(props) {
         comments={[1, 1]}
       />
       <CommentList />
+      <AddComment />
     </div>
   );
 }
--- src/components/FeedBackEmpty.jsx
+++ src/components/FeedBackEmpty.jsx
@@ -1,6 +1,5 @@
 import "./FeedBackEmpty.css";
 import emptyIcon from "../assets/suggestions/illustration-empty.svg";
-import Button from "./Button";
 import plusIcon from "../assets/shared/icon-plus.svg";
 
 function FeedBackEmpty() {
@@ -20,23 +19,18 @@ function FeedBackEmpty() {
           hearing about new ideas to improve our app.
         </p>
       </div>
-      <Button
-        text="Add Feedback"
-        icon={
-          <>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              height="12"
-              viewBox="0 -960 960 960"
-              width="12"
-              fill="#F2F4FE"
-            >
-              <path d="M417-417H166v-126h251v-251h126v251h251v126H543v251H417v-251Z" />
-            </svg>
-          </>
-        }
-        type={1}
-      />
+      <button className="SuggestionsTab_btn">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="12"
+          viewBox="0 -960 960 960"
+          width="12"
+          fill="#F2F4FE"
+        >
+          <path d="M417-417H166v-126h251v-251h126v251h251v126H543v251H417v-251Z" />
+        </svg>
+        <span>Add Feedback</span>
+      </button>
     </div>
   );
 }
