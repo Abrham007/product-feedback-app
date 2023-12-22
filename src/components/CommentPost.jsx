3ba8b72b2f584d6432abee7b20812d95078075ed
Abrham007 <abrhamaraya111@gmail.com>
2023-12-22 13:43:18 +0300

    added functionalty to the button

--- src/components/CommentPost.jsx
+++ src/components/CommentPost.jsx
@@ -1,7 +1,14 @@
 import "./CommentPost.css";
 import userImg from "../assets/user-images/image-elijah.jpg";
+import { useState } from "react";
 
 function CommentPost() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  function toggleReplay() {
+    setIsOpen((prevValue) => !prevValue);
+  }
+
   return (
     <div className="CommentPost">
       <img
@@ -15,13 +22,21 @@ function CommentPost() {
         <h5 className="CommentPost__name">Elijah Moss</h5>
         <p className="CommentPost__email">@hexagon.bestagon</p>
       </div>
-      <button className="CommentPost__btn">Replay</button>
+      <button className="CommentPost__btn-get" onClick={toggleReplay}>
+        Replay
+      </button>
       <p className="CommentPost__description">
         Also, please allow styles to be applied based on system preferences. I
         would love to be able to browse Frontend Mentor in the evening after my
         device’s dark mode turns on without the bright background it currently
         has.
       </p>
+      {isOpen && (
+        <div className="CommentPost__replay">
+          <textarea className="CommentPost__input"></textarea>
+          <button className="CommentPost__btn-post">Post Reply</button>
+        </div>
+      )}
     </div>
   );
 }
