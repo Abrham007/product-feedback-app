e8ff28aff427fa5409c6d79f837248b25546d153
Abrham007 <abrhamaraya111@gmail.com>
2023-12-23 16:44:36 +0300

    corrected the key on fragment bug

--- src/components/CommentList.jsx
+++ src/components/CommentList.jsx
@@ -1,22 +1,36 @@
+import { Fragment } from "react";
 import "./CommentList.css";
 import CommentPost from "./CommentPost";
 import CommentPostWithReplay from "./CommentPostWithReplay";
 
-function CommentList() {
+function CommentList(props) {
   return (
     <div className="CommentList">
-      <h4 className="CommentList__header">4 Comments</h4>
-      <CommentPost />
-      <hr className="CommentList__line"></hr>
-      <CommentPostWithReplay
-        comment={<CommentPost />}
-        replies={
-          <>
-            <CommentPost />
-            <CommentPost />
-          </>
+      <h4 className="CommentList__header"> Comments</h4>
+      {props.list.map((comment) => {
+        if (!comment.replies) {
+          return (
+            <Fragment key={comment.id}>
+              <CommentPost key={comment.id} {...comment} />
+              <hr key={"line" + comment.id} className="CommentList__line"></hr>
+            </Fragment>
+          );
+        } else {
+          let commentReplies = comment.replies?.map((replie, index) => (
+            <CommentPost key={index} {...replie} />
+          ));
+          return (
+            <Fragment key={comment.id}>
+              <CommentPostWithReplay
+                key={comment.id}
+                comment={<CommentPost key={comment.id} {...comment} />}
+                replies={commentReplies}
+              />
+              <hr key={"line" + comment.id} className="CommentList__line"></hr>
+            </Fragment>
+          );
         }
-      />
+      })}
     </div>
   );
 }
