ee9262fd441fa85e19c9a3757e994b26b29425c1
Abrham007 <abrhamaraya111@gmail.com>
2023-12-23 16:43:46 +0300

    made the component interavtive and functional

--- src/components/AddComment.jsx
+++ src/components/AddComment.jsx
@@ -1,16 +1,51 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./AddComment.css";
 
-function AddComment() {
+function AddComment(props) {
+  const [newContent, setNewContent] = useState("");
+  const navigate = useNavigate();
+  const charLeft = 250 - newContent.length;
+
+  function handleChange(event) {
+    const { value } = event.target;
+    setNewContent(value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let newComment = {
+      id: 16,
+      content: newContent,
+      user: props.appData.currentUser,
+    };
+
+    props.handleAppData((prevVaule) => {
+      let feedBackPostIndex = prevVaule.productRequests.findIndex(
+        (req) => req.id == props.id
+      );
+      prevVaule.productRequests[feedBackPostIndex].comments.push(newComment);
+      return prevVaule;
+    });
+
+    navigate(`/feedbackdetail/${props.id}`);
+  }
+
   return (
-    <form className="AddComment">
+    <form onSubmit={handleSubmit} className="AddComment">
       <h4 className="AddComment__header">Add Comment</h4>
       <textarea
         className="AddComment__input"
         placeholder="Type your comment here"
+        value={newContent}
+        onChange={handleChange}
+        maxLength={250}
       ></textarea>
       <div className="AddComment__box">
-        <p className="AddComment__char">250 Characters left</p>
-        <button className="AddComment__btn">Post Comment</button>
+        <p className="AddComment__char">{charLeft} Characters left</p>
+        <button className="AddComment__btn" type="submit">
+          Post Comment
+        </button>
       </div>
     </form>
   );
