8f4f3374f9582db05560c20e10f27903c825928e
Abrham007 <abrhamaraya111@gmail.com>
2023-12-24 11:32:10 +0300

    fix the style bug

--- src/components/CommentPostWithReplay.jsx
+++ src/components/CommentPostWithReplay.jsx
@@ -3,7 +3,10 @@ import "./CommentPostWithReplay.css";
 function CommentPostWithReplay({ comment, replies }) {
   return (
     <div className="CommentPostWithReplay">
-      <hr className="CommentPostWithReplay__line"></hr>
+      <hr
+        className="CommentPostWithReplay__line"
+        style={{ height: `${133 * replies.length}px` }}
+      ></hr>
       <div className="CommentPostWithReplay__comment">{comment}</div>
       <div className="CommentPostWithReplay__replies">{replies}</div>
     </div>
