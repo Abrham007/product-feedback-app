7878eb768e5d894320c9bfc9422fdbf3c1bf0fbf
Abrham007 <abrhamaraya111@gmail.com>
2023-12-24 19:53:58 +0300

    added a react router best practice

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./components/App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 const domNode = document.getElementById("root");
 const root = createRoot(domNode);
-root.render(<App />);
+root.render(
+  <Router>
+    <App />
+  </Router>
+);
