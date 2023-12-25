9cebd79b406c7f94e999a9fcb791026d577558b7
Abrham007 <abrhamaraya111@gmail.com>
2023-12-25 15:24:05 +0300

    finshed the markup and started the style.

---
+++ src/components/RoadMap.jsx
@@ -0,0 +1,22 @@
+import "./RoadMap.css";
+
+function RoadMap() {
+  return (
+    <div className="RoadMap">
+      <div className="RoadMap__header">
+        <div>
+          <button></button>
+          <h2></h2>
+        </div>
+        <button></button>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
+
+export default RoadMap;
