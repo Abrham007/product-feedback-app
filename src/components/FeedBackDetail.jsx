097d3c4c20ee4194d45fb1cae178551603094e70
Abrham007 <abrhamaraya111@gmail.com>
2023-12-26 10:27:33 +0300

    removed work load to another component

--- src/components/FeedBackDetail.jsx
+++ src/components/FeedBackDetail.jsx
@@ -1,13 +1,12 @@
 import "./FeedBackDetail.css";
 import FeedBackPost from "./FeedBackPost";
-import leftArrowIcon from "../assets/shared/icon-arrow-left.svg";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import BackBtn from "./BackBtn";
 
 function FeedBackDetail(props) {
   let { id } = useParams();
-  let navigate = useNavigate();
 
   const FEEDBACKDETAIL = props.appData.productRequests.find(
     (req) => req.id == id
@@ -18,25 +17,8 @@ function FeedBackDetail(props) {
   return (
     <div className="FeedBackDetail">
       <div className="FeedBackDetail__header">
-        <button
-          onClick={() => navigate(-1)}
-          className="FeedBackDetail__link FeedBackDetail__link--1"
-        >
-          <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
-            <path
-              d="M6 9L2 5l4-4"
-              stroke="#647196"
-              strokeWidth="2"
-              fill="none"
-              fillRule="evenodd"
-            />
-          </svg>
-          <span>Go Back</span>
-        </button>
-        <Link
-          to={`/edit/${id}`}
-          className="FeedBackDetail__link FeedBackDetail__link--2"
-        >
+        <BackBtn />
+        <Link to={`/edit/${id}`} className="FeedBackDetail__link">
           Edit Feedback
         </Link>
       </div>
--- src/components/FeedBackEmpty.jsx
+++ src/components/FeedBackEmpty.jsx
@@ -1,7 +1,7 @@
 import "./FeedBackEmpty.css";
 import emptyIcon from "../assets/suggestions/illustration-empty.svg";
-import plusIcon from "../assets/shared/icon-plus.svg";
-import { Link } from "react-router-dom";
+
+import AddBtn from "./AddBtn";
 
 function FeedBackEmpty() {
   return (
@@ -20,18 +20,7 @@ function FeedBackEmpty() {
           hearing about new ideas to improve our app.
         </p>
       </div>
-      <Link to={"/"} className="SuggestionsTab_btn">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="12"
-          viewBox="0 -960 960 960"
-          width="12"
-          fill="#F2F4FE"
-        >
-          <path d="M417-417H166v-126h251v-251h126v251h251v126H543v251H417v-251Z" />
-        </svg>
-        <span>Add Feedback</span>
-      </Link>
+      <AddBtn />
     </div>
   );
 }
