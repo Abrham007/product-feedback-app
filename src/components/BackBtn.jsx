6eb8369d4c8e4fc46be471e4971de965fb204bde
Abrham007 <abrhamaraya111@gmail.com>
2024-01-02 13:45:52 +0300

    corrected a style mistake

--- src/components/BackBtn.jsx
+++ src/components/BackBtn.jsx
@@ -12,7 +12,7 @@ function BackBtn({ color = "#647196", backPage = -1, ...props }) {
       <svg width="7" height="10" xmlns="http://www.w3.org/2000/svg">
         <path
           d="M6 9L2 5l4-4"
-          stroke={color}
+          stroke={"#4661E6"}
           strokeWidth="2"
           fill="none"
           fillRule="evenodd"
