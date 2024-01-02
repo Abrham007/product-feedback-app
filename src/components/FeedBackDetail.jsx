1d12ae9b6c0f1c3ccbf5364b562c2e3840f81f2e
Abrham007 <abrhamaraya111@gmail.com>
2024-01-02 15:47:49 +0300

    removed unused code

--- src/components/FeedBackDetail.jsx
+++ src/components/FeedBackDetail.jsx
@@ -2,12 +2,13 @@ import "./FeedBackDetail.css";
 import FeedBackPost from "./FeedBackPost";
 import CommentList from "./CommentList";
 import AddComment from "./AddComment";
-import { Link, useParams } from "react-router-dom";
+import { Link, useParams, redirect, useNavigate } from "react-router-dom";
 import BackBtn from "./BackBtn";
 import { selectProductRequests } from "../features/productRequests/productRequestsSlice";
 import { useSelector } from "react-redux";
 function FeedBackDetail() {
   let { id } = useParams();
+  let navigate = useNavigate();
 
   const FEEDBACKDETAIL = useSelector((state) =>
     state.productRequests.posts.find((req) => req._id == id)
@@ -18,7 +19,7 @@ function FeedBackDetail() {
   return (
     <div className="FeedBackDetail">
       <div className="FeedBackDetail__header">
-        <BackBtn backPage="/" />
+        <BackBtn />
         <Link to={`/edit/${id}`} className="FeedBackDetail__link">
           Edit Feedback
         </Link>
