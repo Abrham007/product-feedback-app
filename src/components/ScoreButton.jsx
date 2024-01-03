afad833118a8398d631b72d1d64d6c8af0239563
Abrham007 <abrhamaraya111@gmail.com>
2024-01-03 15:23:53 +0300

    improved UX

--- src/components/ScoreButton.jsx
+++ src/components/ScoreButton.jsx
@@ -5,21 +5,30 @@ import { increaseVote } from "../features/productRequests/productRequestsSlice";
 
 function ScoreButton(props) {
   const currentUser = useSelector((state) => state.currentUser.user);
+  const voteStatus = useSelector((state) => state.productRequests.status);
 
   let isClicked = false;
-  if (props.usersWhoVoted?.includes(currentUser)) {
+  if (props.usersWhoVoted.includes(currentUser._id)) {
     isClicked = true;
+  }
+
+  let isLoading = false;
+  if (voteStatus === "loading") {
+    isLoading = true;
   }
   const dispatch = useDispatch();
 
   function handleUpVotes(event) {
     event.preventDefault();
-    dispatch(increaseVote({ postId: props.id, curretnUser: currentUser }));
+    dispatch(increaseVote({ postId: props.id, curretnUser: currentUser._id }));
   }
 
   let btnStyles = {};
+  let svgStyle = { stroke: "#4661E6" };
+
   if (isClicked) {
     btnStyles = { backgroundColor: "#4661e6", color: "#fff" };
+    svgStyle = { stroke: "#fff" };
   }
 
   let btnClass = "ScoreButton";
@@ -29,14 +38,13 @@ function ScoreButton(props) {
 
   return (
     <button
-      style={btnStyles}
+      style={{ ...btnStyles, cursor: isLoading ? "progress" : "pointer" }}
       className={btnClass}
       onClick={handleUpVotes}
       disabled={isClicked}
     >
       <svg
-        style={{ stroke: isClicked ? "#fff" : "#4661E6" }}
-        stroke="#4661E6"
+        style={svgStyle}
         width="10"
         height="7"
         xmlns="http://www.w3.org/2000/svg"
