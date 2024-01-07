7852844402cd2b22cab8a7c359787a697c36a981
Abrham007 <abrhamaraya111@gmail.com>
2024-01-07 12:02:28 +0300

    finished configer to test redux with jest

--- setupTests.js
+++ setupTests.js
@@ -1,3 +1,5 @@
 import React from "react";
-
+import { TextEncoder, TextDecoder } from "util";
+Object.assign(global, { TextDecoder, TextEncoder });
 import "@testing-library/jest-dom";
+import "isomorphic-fetch";
