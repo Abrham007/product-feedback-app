eb1b355e6a54d2f7df69ea376acc37f3ba6a9487
Abrham007 <abrhamaraya111@gmail.com>
2024-01-07 12:03:05 +0300

    finished the RoadMapView test suite

--- __tests__/RoadMapView.test.js
+++ __tests__/RoadMapView.test.js
@@ -1,37 +1,24 @@
 import RoadMapView from "../src/components/RoadMapView";
-import { http, HttpResponse, delay } from "msw/browser";
-import { setupServer } from "msw/node";
 import { fireEvent, screen } from "@testing-library/react";
 import { renderWithProviders } from "../utils/test-utils";
 
-export const handlers = [
-  http.get("http://127.0.0.1:4000/post", async () => {
-    await delay(150);
-    return HttpResponse.json([
-      { status: "planned" },
-      { status: "planned" },
-      { status: "in-progress" },
-      { status: "live" },
-      { status: "live" },
-    ]);
-  }),
-];
-
-const server = setupServer(...handlers);
-
-// Enable API mocking before tests.
-beforeAll(() => server.listen());
-
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers());
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
-
 describe("RoadMapView", () => {
-  it("loads the correct infromation from state", () => {
+  beforeEach(() => {
     renderWithProviders(<RoadMapView></RoadMapView>);
-
-    console.log(screen.getByRole("ul"));
+  });
+  it("loads the correct infromation from state", () => {
+    expect(
+      screen.getAllByRole("listitem")[0].textContent.match(/\d+$/)[0]
+    ).toBe("2");
+    expect(
+      screen.getAllByRole("listitem")[1].textContent.match(/\d+$/)[0]
+    ).toBe("3");
+    expect(
+      screen.getAllByRole("listitem")[2].textContent.match(/\d+$/)[0]
+    ).toBe("1");
+  });
+  it("changes to RoadMap page when the link is clicked", () => {
+    fireEvent.click(screen.getByRole("link"));
+    expect(screen.getByRole("heading").textContent).toBe("Roadmap");
   });
 });
