967984eba2d04ff9d3b95268d17d55b8805e1531
Abrham007 <abrhamaraya111@gmail.com>
2024-01-08 15:40:26 +0300

    changed the initial state to match data in database

--- utils/test-utils.jsx
+++ utils/test-utils.jsx
@@ -10,6 +10,7 @@ export function renderWithProviders(
     preloadedState = {
       currentUser: {
         user: {
+          _id: 1,
           image: "./assets/user-images/image-zena.jpg",
           name: "Zena Kelley",
           username: "velvetround",
@@ -18,16 +19,17 @@ export function renderWithProviders(
       productRequests: {
         posts: [
           {
-            id: 1,
+            _id: 1,
             title: "Add tags for solutions",
             category: "enhancement",
             upvotes: 112,
             status: "suggestion",
+            usersWhoVoted: [],
             description:
               "Easier to search for solutions based on a specific stack.",
             comments: [
               {
-                id: 1,
+                _id: 1,
                 content:
                   "Awesome idea! Trying to find framework-specific projects within the hubs can be tedious",
                 user: {
@@ -37,7 +39,7 @@ export function renderWithProviders(
                 },
               },
               {
-                id: 2,
+                _id: 2,
                 content:
                   "Please use fun, color-coded labels to easily identify them at a glance",
                 user: {
@@ -49,11 +51,12 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 2,
+            _id: 2,
             title: "Add a dark theme option",
             category: "feature",
             upvotes: 99,
             status: "suggestion",
+            usersWhoVoted: [],
             description:
               "It would help people with light sensitivities and who prefer dark mode.",
             comments: [
@@ -68,7 +71,7 @@ export function renderWithProviders(
                 },
               },
               {
-                id: 4,
+                _id: 4,
                 content:
                   "Second this! I do a lot of late night coding and reading. Adding a dark theme can be great for preventing eye strain and the headaches that result. It’s also quite a trend with modern apps and  apparently saves battery life.",
                 user: {
@@ -102,16 +105,17 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 3,
+            _id: 3,
             title: "Q&A within the challenge hubs",
             category: "feature",
             upvotes: 65,
             status: "suggestion",
+            usersWhoVoted: [],
             description:
               "Challenge-specific Q&A would make for easy reference.",
             comments: [
               {
-                id: 5,
+                _id: 5,
                 content:
                   "Much easier to get answers from devs who can relate, since they've either finished the challenge themselves or are in the middle of it.",
                 user: {
@@ -123,16 +127,17 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 4,
+            _id: 4,
             title: "Add image/video upload to feedback",
             category: "enhancement",
             upvotes: 51,
             status: "suggestion",
+            usersWhoVoted: [],
             description:
               "Images and screencasts can enhance comments on solutions.",
             comments: [
               {
-                id: 6,
+                _id: 6,
                 content:
                   "Right now, there is no ability to add images while giving feedback which isn't ideal because I have to use another app to show what I mean",
                 user: {
@@ -142,7 +147,7 @@ export function renderWithProviders(
                 },
               },
               {
-                id: 7,
+                _id: 7,
                 content:
                   "Yes I'd like to see this as well. Sometimes I want to add a short video or gif to explain the site's behavior..",
                 user: {
@@ -154,16 +159,17 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 5,
+            _id: 5,
             title: "Ability to follow others",
             category: "feature",
             upvotes: 42,
             status: "suggestion",
+            usersWhoVoted: [],
             description:
               "Stay updated on comments and solutions other people post.",
             comments: [
               {
-                id: 8,
+                _id: 8,
                 content:
                   "I also want to be notified when devs I follow submit projects on FEM. Is in-app notification also in the pipeline?",
                 user: {
@@ -185,7 +191,7 @@ export function renderWithProviders(
                 ],
               },
               {
-                id: 9,
+                _id: 9,
                 content:
                   "I've been saving the profile URLs of a few people and I check what they’ve been doing from time to time. Being able to follow them solves that",
                 user: {
@@ -197,25 +203,27 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 6,
+            _id: 6,
             title: "Preview images not loading",
             category: "bug",
             upvotes: 3,
             status: "suggestion",
+            usersWhoVoted: [],
             description:
               "Challenge preview images are missing when you apply a filter.",
           },
           {
-            id: 7,
+            _id: 7,
             title: "More comprehensive reports",
             category: "feature",
             upvotes: 123,
             status: "planned",
+            usersWhoVoted: [],
             description:
               "It would be great to see a more detailed breakdown of solutions.",
             comments: [
               {
-                id: 10,
+                _id: 10,
                 content:
                   "This would be awesome! It would be so helpful to see an overview of my code in a way that makes it easy to spot where things could be improved.",
                 user: {
@@ -225,7 +233,7 @@ export function renderWithProviders(
                 },
               },
               {
-                id: 11,
+                _id: 11,
                 content:
                   "Yeah, this would be really good. I'd love to see deeper insights into my code!",
                 user: {
@@ -237,16 +245,17 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 8,
+            _id: 8,
             title: "Learning paths",
             category: "feature",
             upvotes: 28,
             status: "planned",
+            usersWhoVoted: [],
             description:
               "Sequenced projects for different goals to help people improve.",
             comments: [
               {
-                id: 12,
+                _id: 12,
                 content:
                   "Having a path through the challenges that I could follow would be brilliant! Sometimes I'm not sure which challenge would be the best next step to take. So this would help me navigate through them!",
                 user: {
@@ -258,16 +267,17 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 9,
+            _id: 9,
             title: "One-click portfolio generation",
             category: "feature",
             upvotes: 62,
             status: "in-progress",
+            usersWhoVoted: [],
             description:
               "Add ability to create professional looking portfolio from profile.",
             comments: [
               {
-                id: 13,
+                _id: 13,
                 content:
                   "I haven't built a portfolio site yet, so this would be really helpful. Might it also be possible to choose layout and colour themes?!",
                 user: {
@@ -279,7 +289,7 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 10,
+            _id: 10,
             title: "Bookmark challenges",
             category: "feature",
             upvotes: 31,
@@ -287,7 +297,7 @@ export function renderWithProviders(
             description: "Be able to bookmark challenges to take later on.",
             comments: [
               {
-                id: 14,
+                _id: 14,
                 content:
                   "This would be great! At the moment, I'm just starting challenges in order to save them. But this means the My Challenges section is overflowing with projects and is hard to manage. Being able to bookmark challenges would be really helpful.",
                 user: {
@@ -299,24 +309,26 @@ export function renderWithProviders(
             ],
           },
           {
-            id: 11,
+            _id: 11,
             title: "Animated solution screenshots",
             category: "bug",
             upvotes: 9,
             status: "in-progress",
+            usersWhoVoted: [],
             description:
               "Screenshots of solutions with animations don’t display correctly.",
           },
           {
-            id: 12,
+            _id: 12,
             title: "Add micro-interactions",
             category: "enhancement",
             upvotes: 71,
             status: "live",
+            usersWhoVoted: [],
             description: "Small animations at specific points can add delight.",
             comments: [
               {
-                id: 15,
+                _id: 15,
                 content:
                   "I'd love to see this! It always makes me so happy to see little details like these on websites.",
                 user: {
