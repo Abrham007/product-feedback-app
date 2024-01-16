0a05b019233d9ddd1846364cdee394ba894fff4d
Abrham007 <abrhamaraya111@gmail.com>
2024-01-16 13:38:11 +0300

    changed the fetch polyfill

--- setupTests.js
+++ setupTests.js
@@ -2,4 +2,4 @@ import React from "react";
 import { TextEncoder, TextDecoder } from "util";
 Object.assign(global, { TextDecoder, TextEncoder });
 import "@testing-library/jest-dom";
-import "isomorphic-fetch";
+import "whatwg-fetch";
